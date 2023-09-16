13b10c7e1035f33a9f0d2e19b06ba19f9c350e4a
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-09-16 19:33:44 +0000

    Add a navigation module that currently holds the general stack navigator implemented using react-navigation

---
+++ src/navigation/GeneralNav.jsx
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+import { createNativeStackNavigator } from '@react-navigation/native-stack'
+import Onboarding from '../screens/Onboarding'
+import Register from '../screens/Register'
+import Login from '../screens/Login'
+
+export default function Main() {
+     const NativeStack = createNativeStackNavigator()
+     
+     return (
+          <NativeStack.Navigator
+               initialRouteName='Onboarding'
+               screenOptions={() => ({
+                    header: () => null
+               })}
+          >
+               <NativeStack.Screen 
+                    name='Onboarding' 
+                    component={Onboarding} 
+               />
+               <NativeStack.Screen 
+                    name='Register' 
+                    component={Register} 
+               />
+               <NativeStack.Screen 
+                    name='Login' 
+                    component={Login} 
+               />
+          </NativeStack.Navigator>
+    )
+}
+
+const styles = StyleSheet.create({})

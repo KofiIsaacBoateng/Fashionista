18b3adc7c0e4c2107ef9b75a1a74e1322c737cf8
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2023-09-17 21:34:19 +0000

    Added the onboarding slider for fashionista... Left with the remaining content

--- src/components/Slide.jsx
+++ src/components/Slide.jsx
@@ -1,16 +1,44 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
+const {width, height}  = Dimensions.get("window")
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      width
+    },
+
+    textContainer: {
+      height: 100,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+
+    title: {
+      fontSize: 75,
+      color: '#fff',
+      fontWeight: 'bold',
+      letterSpacing: 3
+    }
+})
+
 const Slide = ({label, right}) => {
+  const transform ={
+    transform: [
+      {translateY: height / 2 - 150},
+      {translateX: right ? width / 2  - 50 : -width / 2 + 50},
+      {rotate: right ? '90deg' : '-90deg' }
+    ]
+  }  
+
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>{label}</Text>
+        <View style={[styles.textContainer, transform]}>
+          <Text style={styles.title}>{label}</Text>
+        </View>
     </View>
   )
 }
 
 export default Slide
 
-const styles = StyleSheet.create({
-     
-})
--- src/screens/Onboarding.jsx
+++ src/screens/Onboarding.jsx
@@ -1,9 +1,10 @@
 import { Dimensions, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
-import Slide from '../../components/Slide'
-import { ScrollView } from 'react-native-gesture-handler'
+import React, { useRef } from 'react'
+import Slide from '../components/Slide'
+import Animated, { useAnimatedScrollHandler, interpolateColor, useSharedValue, useAnimatedStyle } from 'react-native-reanimated'
 
 
+const {width, height} = Dimensions.get("window")
 const styles = StyleSheet.create({
   container: {
       flex: 1,
@@ -11,36 +12,66 @@ const styles = StyleSheet.create({
       borderBottomLeftRadius: 75
   },
   slider: {
-      flex: height * 0.61,
+      height: 0.31 * height,
       backgroundColor: 'cyan',
+      borderBottomRightRadius: 75
   },
 
   captions: {
       flex: 1,
       backgroundColor: '#fff'
+  },
+
+  underlay: {
+    flex: 1
+  },
+
+  overlay: {
+    flex: 1,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 75
   }
 
 })
 
 const Onboarding = () => {
-  const {width, height} = Dimensions.get("window")
+  let count = 0
+  const x = useSharedValue(count)
+  const onScroll = useAnimatedScrollHandler({
+    onScroll: (event) => {
+      x.value = event.contentOffset.x
+    }
+  })
+  const backgroundColor = useAnimatedStyle(() => {
+    return {
+      backgroundColor: interpolateColor(x.value,
+        [0, width, width * 2, width * 3],
+        ['#bfeaf5', '#beecc4', '#ffe4d9', '#ffdddd']
+      )
+    }
+  }
+  )
+  
   return (
-    <View style={[{flex: 1}]}>
-      <ScrollView 
-        style={styles.slider}
+    <View style={[styles.container, {flex: 1}]}>
+      <Animated.ScrollView 
+        style={[styles.slider, backgroundColor]}
         horizontal
         snapToInterval={width}
-        bounces={true}
+        bounces={false}
+        showsHorizontalScrollIndicator={false}
+        decelerationRate='fast'
+        onScroll={onScroll}
       >
           <Slide label='Fantastic' />
           <Slide label='Eccentric' right />
-          <Slide label='Look Good'/>
-          <Slide label='Superfly' right/>
-      </ScrollView>
+          <Slide label='Relaxed'/>
+          <Slide label='Flirty' right/>
+      </Animated.ScrollView>
 
       <View style={styles.captions}>
-          <View style={styles.underlay}></View>
-          <View style={styles.overlay}></View>
+          <Animated.View style={[styles.underlay, {...StyleSheet.absoluteFillObject,}, backgroundColor]}></Animated.View>
+          <Animated.View style={styles.overlay}></Animated.View>
       </View>
     </View>
   )

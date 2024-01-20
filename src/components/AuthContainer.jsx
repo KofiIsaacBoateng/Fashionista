54e1ce9120760d1e301e8182944b17c5af67f425
KofiIsaacBoateng <kofiisaacboateng@gmail.com>
2024-01-20 14:39:40 +0000

    update: authentication screens layout design

--- src/components/AuthContainer.jsx
+++ src/components/AuthContainer.jsx
@@ -1,28 +1,80 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Dimensions, Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import { SafeAreaView } from 'react-native-safe-area-context'
-import { LinearGradient } from 'expo-linear-gradient'
+import FooterSocialLogins from './Footer'
+
+const {width, height} = Dimensions.get('window')
+const styles = StyleSheet.create({
+  header: {
+    backgroundColor: '#fff',
+    width,
+  },
+
+  headerImage: {
+    width,
+    height: "100%",
+    borderBottomRightRadius: 75,
+  },
+  
+  underlay: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  
+  underlayImage: {
+    transform: [
+      {"rotate": "180deg"}
+    ]
+  }, 
+
+  content: {
+    flex: 1,
+    backgroundColor: "#0c0d34"
+  },
+  overlay: {
+    backgroundColor: "#fff",
+    height: "100%",
+    borderTopLeftRadius: 95,
+    borderBottomLeftRadius: 95,
+    borderBottomRightRadius: 95
+  },
+})
 
 const AuthContainer = ({children}) => {
+  const patterns = [
+    require("../../assets/auth/pattern-01.jpg"),
+    require("../../assets/auth/pattern-02.jpg"),
+    require("../../assets/auth/pattern-03.jpg")
+  ]
   return (
-      <SafeAreaView style={{flex: 1}}>
-        <LinearGradient
-          colors={['#4c669f', 'red', 'blue']}
-          style={{flex: 1}}
-        >
-          <View style={styles.container}>
-              {children}
+      <SafeAreaView style={[{flex: 1, backgroundColor: "#fff"}]}>
+          <View style={[styles.header, {flex: 0.15}]}>
+              <Image 
+                source={patterns[0]}
+                style={[styles.headerImage]}
+              />
           </View>
-        </LinearGradient>
+          <View style={styles.content}>
+              <View style={[styles.underlay]}>
+                <Image 
+                  source={patterns[0]}
+                  style={[styles.underlayImage, {height: "20%", width}]}
+                />
+              </View>
+              <View style={styles.overlay}>
+                {children}
+              </View>
+          </View>
+          <View style={{flex: 0.2}}>
+            <View style={[styles.underlay, {backgroundColor: "#0c0d34"}]} />
+            <FooterSocialLogins />
+            <TouchableOpacity style={{justifyContent: "center", flexDirection: "row", gap: 10}}>
+              <Text style={{color: "#fff"}}>Already have an account ?</Text>
+              <Text style={{color: "darkgreen"}}>Sign Up.</Text>
+            </TouchableOpacity>
+          </View>
       </SafeAreaView>
     
   )
 }
 
 export default AuthContainer
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    }
-})
--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,13 +1,12 @@
 import { SafeAreaView, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import AuthContainer from '../components/AuthContainer'
-import { SafeAreaFrameContext } from 'react-native-safe-area-context';
 
 
 const Login = () => {
   return (
     <AuthContainer>
-        <Text style={{fontSize: 55, color: '#fff'}}>Welcome to the Login Page!</Text>
+        <Text style={{fontSize: 23, color: '#000', textAlign: 'center'}}>Login Page</Text>
     </AuthContainer>
   )
 }
